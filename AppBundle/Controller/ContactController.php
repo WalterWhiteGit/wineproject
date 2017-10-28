f7632ab35c7bba4a2c1432edec86a368dcf7bcfa
WalterWhiteGit <32126283+WalterWhiteGit@users.noreply.github.com>
2017-10-29 01:51:12 +0200

    Update ContactController.php

--- AppBundle/Controller/ContactController.php
+++ AppBundle/Controller/ContactController.php
@@ -7,8 +7,11 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
