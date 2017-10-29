53423c195ef8eb5444aaa0827991faefbaa9cd99
WalterWhiteGit <32126283+WalterWhiteGit@users.noreply.github.com>
2017-10-29 14:46:06 +0100

    Update ContactType.php

--- Form/ContactType.php
+++ Form/ContactType.php
@@ -3,8 +3,16 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ContactType extends AbstractType
 {
@@ -13,12 +21,90 @@ class ContactType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('firstname')
-                ->add('lastname')
-                ->add('email')
-                ->add('phonenumber')
-                ->add('content')
-                ->add('object');
+        
+// Button Radio Field
+        
+        $builder->add('object',ChoiceType::class,[
+
+            'choices'=>[
+                        'Informations'=>'INF',
+                        'Réservations'=>'RES',
+                        'Réclamations'=>'REC'
+                       ],
+            'choices_as_values'=>true,'multiple'=>false,'expanded'=>true
+
+
+        ])
+// Firstname Field
+            
+                ->add('firstname',TextType::class,
+                    [
+                        'constraints'=>[new NotBlank(['message'=>'*Le champ prénom est obligatoire']),
+                                        new Length(['min'=>3,
+                                                    'max'=>40,
+                                                    'minMessage'=>'Votre prénom doit contenir au moins {{ limit }} caractères',
+                                                    'maxMessage'=>'Votre prénom doiy être inférieur à {{ limit }} caractères'
+                                                   ]),
+                                        new Regex(['pattern'=>'/^[a-zA-Z]+$/i',
+                                                   'message'=>"Votre prénom doit contenir uniquement des lettres"
+                                                  ])
+                                       ]
+                    ])
+
+// Lastname Field
+                ->add('lastname', TextType::class,
+                    [
+                        'constraints'=>[new NotBlank(['message'=>'*Le champ nom est obligatoire']),
+                                        new Length  (['min'=>3,
+                                                    'max'=>40,
+                                                    'minMessage'=>'Votre nom doit contenir au moins {{ limit }} caractères',
+                                                    'maxMessage'=>'Votre nom doit être inférieur à {{ limit }}'
+                                                    ]),
+                                        new Regex   (['pattern'=>'/^[a-zA-Z]+$/i',
+                                                      'message'=>"Votre nom doit contenir que des lettres"
+                                                    ])
+                                       ]
+                    ])
+
+
+// Email Field
+                ->add('email', EmailType::class,
+                    [
+                        'constraints'=>[new NotBlank(['message'=>'*Le champ email doit être rempli']),
+                                        new Length  (['min'=>6,
+                                                      'minMessage'=>'Votre adresse email est trop courte'
+                                                     ]),
+                                        new Email   (['checkHost'=>true,
+                                                      'checkMX'  =>true
+                                                    ])
+                                       ]
+                    ])
+
+// PhoneNumber Field
+            
+                ->add('phonenumber', TextType::class,
+                       [
+                           'constraints'=>[new NotBlank(['message'=>"Vous devez saisir votre numéro d\'appel"]),
+                                           new Length  (['min'=>10,
+                                                         'max'=>10,
+                                                         'minMessage'=>'Le numéro doit contenir {{ limit }} chiffres',
+                                                         'maxMessage'=>'Le numéro doit contenir {{ limit }} chiffres'
+                                                        ]),
+                                           new Regex    (['pattern'=>'/^(\(0\))?[0-9]+$/',
+                                                          'message'=>'Le numéro doit contenir que des chiffres'
+                                                        ])
+                                            ]
+                       ])
+
+// Message Field
+                ->add('content', TextareaType::class,
+                      [
+                          'constraints'=>[new NotBlank(['message'=>"Veuillez saisir votre message"]),
+                                          new Length  (['min'=>200,
+                                                        'minMessage'=>'Votre message est trop court'
+                                                       ])
+                                         ]
+                      ]);
     }
     
     /**
