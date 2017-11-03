2e9cb6242ffbd0b5e405c64756be1dcb27274624
WalterWhiteGit <32126283+WalterWhiteGit@users.noreply.github.com>
2017-11-03 12:49:37 +0100

    Update ShoppingController.php

--- AppBundle/Controller/shop/ShoppingController.php
+++ AppBundle/Controller/shop/ShoppingController.php
@@ -8,8 +8,14 @@
 
 namespace AppBundle\Controller\shop;
 
+
+
+use AppBundle\Entity\Products;
+use AppBundle\Form\ProductsType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ShoppingController extends Controller
 {
@@ -20,15 +26,57 @@ class ShoppingController extends Controller
      *
      */
 
-   public function shoppingController(){
+
+    public function shoppingController(){
 
         $doctrine = $this->getDoctrine();
 
+
         $result = $doctrine->getRepository(Products::class)->selectAll();
 
+        $em = $doctrine->getManager();
+
+        // Select Entity
+
+        $entity = new Products();
+
+        $entityType = ProductsType::class;
+
+        // Create Form
+
+        $form = $this->createForm($entityType,$entity);
+
+        $createform = $form->createView();
 
         //exit(dump($result));
 
-        return $this->render(':Shop:shopping.html.twig',['results'=>$result]);
+        return $this->render(':Shop:shopping.html.twig',['results'=>$result, 'form'=>$createform]);
     }
+
+
+    /**
+     * @Route("/ajax", name="shop.shopping.ajax")
+     *
+     */
+
+
+    public function ajaxAction(Request $request)
+    {
+        $name =$request->request->get('data');
+
+
+
+        $get= $this->getDoctrine();
+
+        $results = $get->getRepository(Products::class)->searchProducts($name);
+
+
+
+        //Retour ce fait en JSON
+
+        return new JsonResponse($results);
+
+    }
+
+
 }
